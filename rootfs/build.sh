2af6cb07b7675ae4be773b0ecb0ada9edc98639d
Liam Prosser <heofonsearch.help@outlook.com>
2026-09-18 16:45:50 -0500

    Add compilation step for Ceder Linux init

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -13,4 +13,8 @@ mkdir -p rootfs/tmp
 mkdir -p rootfs/usr/bin
 mkdir -p rootfs/var
 
+echo "Compiling Ceder Linux init..."
+
+gcc rootfs/init.c -o rootfs/init
+
 echo "Ceder Linux root filesystem prepared."
